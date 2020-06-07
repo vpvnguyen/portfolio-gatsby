3f9d2a97b7557dbbfeba0f7c7751c8fea1841f05
vpvnguyen <vpvnguyen@gmail.com>
2020-06-07 14:51:42 -0700

    create express server; enabled json and cors

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", require("./controller/projects.controller"));
+
+app.get("/", async (req, res) => {
+  try {
+    res.status(200).json({ message: `Welcome to the Portfolio server!` });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500);
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`portfolio-server is running on localhost:${PORT}`)
+);
