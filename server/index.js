e0a6404f94080eb9bbec4aa8b79cb3b4090e6f34
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-09 18:52:35 -0700

    add time.controller

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use("/time", require("./controller/time.controller"));
 app.use("/api", require("./controller/projects.controller"));
 
 app.get("/", async (req, res) => {
