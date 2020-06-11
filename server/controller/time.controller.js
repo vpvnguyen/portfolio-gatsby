a87a41fb61b48b037b054375b99adf801387de06
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-10 18:56:05 -0700

    send time data

--- server/controller/time.controller.js
+++ server/controller/time.controller.js
@@ -4,16 +4,28 @@ const moment = require("moment");
 
 router.get("/moment", async (req, res) => {
   try {
-    console.log(moment.now());
-    console.log("");
-    console.log(moment.locales());
-    console.log("");
-    console.log(moment().format("MMMM Do YYYY, h:mm:ss a")); // June 9th 2020, 6:48:18 pm
-    console.log(Date.now());
+    console.log(`moment.now(): ${moment.now()}`);
+    console.log(`moment.locales(): ${moment.locales()}`);
+    console.log(
+      `moment().format("MMMM Do YYYY, h:mm:ss a"): ${moment().format(
+        "MMMM Do YYYY, h:mm:ss a"
+      )}`
+    ); // June 9th 2020, 6:48:18 pm
+    console.log(`Date.now(): ${Date.now()}`);
     // moment().format("dddd"); // Tuesday
     // moment().format("MMM Do YY"); // Jun 9th 20
     // moment().format("YYYY [escaped] YYYY"); // 2020 escaped 2020
     // moment().format();
+    const utcDate = new Date().getTimezoneOffset();
+    console.log(`utcDate: ${utcDate}`);
+
+    res.status(200).json({
+      momentNow: moment.now(),
+      momentLocales: moment.locales(),
+      momentFormat: moment().format("MMMM Do YYYY, h:mm:ss a"),
+      dateNow: Date.now(),
+      utcDate: utcDate,
+    });
   } catch (error) {
     console.error(error.message);
     res.status(500);
