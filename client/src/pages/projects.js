a12529ccb0829c56843fd8e92904f842df265493
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-13 12:21:22 -0700

    create projects page

---
+++ client/src/pages/projects.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// components
+import Layout from "../ui/Layout/Layout.js";
+
+const Projects = () => {
+  return (
+    <Layout>
+      <h1>This is the projects page</h1>
+      <p>Render projects here</p>
+    </Layout>
+  );
+};
+
+export default Projects;
