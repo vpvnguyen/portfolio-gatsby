34ec467528340541688d85d381995701649200ea
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-13 12:21:38 -0700

    link to github and linkedin

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,12 +1,16 @@
-import React from "react"
-import { Link } from "gatsby"
-import { makeStyles } from "@material-ui/core/styles"
-import AppBar from "@material-ui/core/AppBar"
-import Toolbar from "@material-ui/core/Toolbar"
-import Typography from "@material-ui/core/Typography"
-import Button from "@material-ui/core/Button"
-import IconButton from "@material-ui/core/IconButton"
-import MenuIcon from "@material-ui/icons/Menu"
+import React from "react";
+import { Link } from "gatsby";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+// social icons
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -23,33 +27,39 @@ const useStyles = makeStyles(theme => ({
     },
     textDecoration: "none" /* no underline */,
   },
-}))
+}));
 
 const Navbar = () => {
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
           <Button variant="raised" className={classes.title} disableRipple>
             <Link to="/" className={classes.title}>
               Vincent Nguyen
             </Link>
           </Button>
-          <Button color="inherit">Resume</Button>
+          <Button color="inherit">
+            <a href="https://www.github.com/vpvnguyen" target="_blank">
+              <GitHubIcon />
+            </a>
+          </Button>
+          <Button color="inherit">
+            <a href="https://www.linkedin.com/in/vpvnguyen" target="_blank">
+              <LinkedInIcon />
+            </a>
+          </Button>
+          <Button color="inherit">
+            <a href="#" target="_blank">
+              Resume
+            </a>
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
