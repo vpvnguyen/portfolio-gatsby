3ec189081affaf84dd4eef49afb52758f904c4b4
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-13 12:22:35 -0700

    add material ui container

--- client/src/ui/Layout/Layout.js
+++ client/src/ui/Layout/Layout.js
@@ -1,22 +1,30 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Navbar from "../../components/Navbar/Navbar.js"
+import React from "react";
+import PropTypes from "prop-types";
+
+// material ui
+import { Container } from "@material-ui/core";
+
+// components
+import Navbar from "../../components/Navbar/Navbar.js";
+
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
-      <main>{children}</main>
+      <Container maxWidth="sm">
+        <main>{children}</main>
+      </Container>
       <footer>
         © {new Date().getFullYear()}, Built with
         {` `}
         <a href="https://www.gatsbyjs.org">Gatsby</a>
       </footer>
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
