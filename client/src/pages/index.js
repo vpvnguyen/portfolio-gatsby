046fd0dfdca233c372e3d6a5c608046a0787d44a
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-13 12:22:19 -0700

    link to projects page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,15 +1,15 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../ui/Layout/Layout.js"
-import Image from "../images/image.js"
+import Layout from "../ui/Layout/Layout.js";
+import Image from "../images/image.js";
 
 const style = {
   defaultImage: {
     maxWidth: `300px`,
     marginBottom: `1.45rem`,
   },
-}
+};
 
 const IndexPage = () => (
   <Layout>
@@ -24,7 +24,9 @@ const IndexPage = () => (
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     <br />
     <Link to="/time/">See Server Time</Link>
+    <br />
+    <Link to="/projects/">Projects</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
