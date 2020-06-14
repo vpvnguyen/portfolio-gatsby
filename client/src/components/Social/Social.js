780a3dcd695191faac97497634b687acbc5dc3e8
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-14 11:13:32 -0700

    create social component

---
+++ client/src/components/Social/Social.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+// social icons
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+
+const socialMedia = {
+  user: "vpvnguyen",
+  github: {
+    url: user => `https://www.github.com/${user}`,
+  },
+  linkedin: {
+    url: user => `https://www.linkedin.com/in/${user}`,
+  },
+};
+
+const style = {
+  fontSize: "1.4em",
+  margin: "0.2em",
+};
+
+const Social = () => {
+  return (
+    <>
+      <a
+        style={style}
+        href={socialMedia.github.url(socialMedia.user)}
+        rel="noreferrer"
+        target="_BLANK"
+      >
+        <GitHubIcon />
+      </a>
+      <a
+        style={style}
+        href={socialMedia.linkedin.url(socialMedia.user)}
+        rel="noreferrer"
+        target="_BLANK"
+      >
+        <LinkedInIcon />
+      </a>
+    </>
+  );
+};
+
+export default Social;
