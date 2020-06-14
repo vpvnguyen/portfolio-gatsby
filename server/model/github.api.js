fe8c74cf262a0f47748f4a009afd16cfc9b8dcdc
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-13 22:30:01 -0700

    query projects for 100 pages

--- server/model/github.api.js
+++ server/model/github.api.js
@@ -3,8 +3,9 @@ const axios = require("axios");
 const githubApi = {
   getGithubProjects: async () => {
     try {
+      // get repos; 30 results per page; set per_page=100 to query for 100 pages;
       const response = await axios.get(
-        `https://api.github.com/users/vpvnguyen/repos`
+        `https://api.github.com/users/vpvnguyen/repos?per_page=100`
       );
       return response.data;
     } catch (error) {
