e2f018dac44503942a2bf1fbe41dad6711e75bf7
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-16 20:07:56 -0700

    filled navbar to screen; navbar hover shadow effect

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -7,6 +7,9 @@ import Button from "@material-ui/core/Button";
 // social icons
 import Social from "../Social/Social.js";
 
+// effects
+import ElevationScroll from "./Navbar.effects.js";
+
 const style = {
   root: {
     flexGrow: 1,
@@ -24,21 +27,29 @@ const style = {
 const Navbar = () => {
   return (
     <div style={style.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Button variant="raised" style={style.title} disableRipple>
-            <Link to="/" style={style.title}>
-              Vincent Nguyen
-            </Link>
-          </Button>
-          <Social />
-          <Button color="inherit">
-            <a href="#" target="_blank">
-              Resume
-            </a>
-          </Button>
-        </Toolbar>
-      </AppBar>
+      {/* slight shadow effect for navbar when scrolling */}
+      <ElevationScroll>
+        {/* position="fixed" will fill bar to edges; keep bar fixed to top of screen when scrolling */}
+        <AppBar position="fixed">
+          <Toolbar style={style.root}>
+            <Button variant="raised" style={style.title} disableRipple>
+              <Link to="/" style={style.title}>
+                Vincent Nguyen
+              </Link>
+            </Button>
+            <Social />
+            <Button color="inherit">
+              <a
+                href="https://docs.google.com/document/d/1PTogfVNXdTCEIQor0yN4gXIcFEKxHRFzGIlRl4287xM/edit?usp=sharing"
+                target="_blank"
+                download
+              >
+                Resume
+              </a>
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
     </div>
   );
 };
