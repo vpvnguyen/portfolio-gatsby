b66fd0a8e8b144a43c4496dc9ed696b015af89f8
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-20 21:54:43 -0700

    Configuring pages with matchPath

--- client/gatsby-node.js
+++ client/gatsby-node.js
@@ -5,3 +5,19 @@
  */
 
 // You can delete this file if you're not using it
+
+// Implement the Gatsby API “onCreatePage”. This is
+// called after every page is created.
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions;
+
+  // Only update the `/app` page.
+  if (page.path.match(/^\/dynamic/)) {
+    // page.matchPath is a special key that's used for matching pages
+    // with corresponding routes only on the client.
+    page.matchPath = "/dynamic/*";
+
+    // Update the page.
+    createPage(page);
+  }
+};
