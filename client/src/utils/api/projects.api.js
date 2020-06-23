016f480c8ea8fa66cb082ac87f1d6b383492b5dc
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-22 18:19:08 -0700

    created request for uploading a project

--- client/src/utils/api/projects.api.js
+++ client/src/utils/api/projects.api.js
@@ -2,6 +2,13 @@ import axios from "axios";
 
 const ProjectsAPI = {
   getAllProjects: () => axios.get("http://localhost:5000/api/getAllProjects"),
+  uploadProject: ({ githubUrl, title, description, demoUrl }) =>
+    axios.post(`http://localhost:5000/api/upload-project`, {
+      githubUrl,
+      title,
+      description,
+      demoUrl,
+    }),
 };
 
 export default ProjectsAPI;
