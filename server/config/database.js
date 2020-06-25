31b1f27a1b4163fee80d4530488a99e2c1c570a6
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-24 20:37:12 -0700

    reinstalled dotenv, get DB creds from dotenv

--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,12 @@
 const { Pool } = require("pg");
-require("dot-env");
+require("dotenv").config();
 
 const pool = new Pool({
-  user: DB_USER,
-  host: DB_HOST,
-  port: DB_PORT,
-  database: DB_DATABASE,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
 });
 
 module.exports = pool;
