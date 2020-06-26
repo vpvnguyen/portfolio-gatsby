c944c8f9430e4ffca385b45a64ad21aa07ce20da
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-25 21:53:41 -0700

    create Welcome component to display on index

---
+++ client/src/components/Welcome/Welcome.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <>
+      <h1>Welcome component</h1>
+
+      <h1>Hi, I'm Vincent :)</h1>
+      <p>Welcome to my Portfolio.</p>
+      <p>Now go build something great.</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+    </>
+  );
+};
+
+export default Welcome;
