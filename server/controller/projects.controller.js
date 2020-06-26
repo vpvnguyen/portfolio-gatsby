edd3b605a855664c75764948557d27c3e9a2f238
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-25 21:54:34 -0700

    delete a project

--- server/controller/projects.controller.js
+++ server/controller/projects.controller.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { getGithubProjects } = require("../model/github.api.js");
-const { getAllProjects, addProject } = require("../model/projects.db.js");
+const {
+  getAllProjects,
+  addProject,
+  deleteProject,
+} = require("../model/projects.db.js");
+const { default: Axios } = require("axios");
 
 // get all projects
 router.get("/projects", async (req, res) => {
@@ -16,8 +21,19 @@ router.get("/projects", async (req, res) => {
 
 // upload a project
 router.post("/upload-project", async (req, res) => {
+  console.log("/upload-project");
   try {
     const { title, description, githubUrl, demoUrl } = req.body;
+    console.log("req.body", req.body);
+    console.log("title", typeof title);
+
+    if (title === "" || description === "" || githubUrl === "") {
+      console.log("something is empty");
+      return res.json({
+        error: "Title, Description, Github URL should not be empty",
+      });
+    }
+
     // add project using ORM
     const newProject = await addProject(title, description, githubUrl, demoUrl);
     res.status(200).json(newProject);
@@ -25,6 +41,20 @@ router.post("/upload-project", async (req, res) => {
   } catch (error) {
     console.error("/upload-project", error.message);
     res.status(500);
+  }
+});
+
+// delete project by ID
+router.delete("/delete-project/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(`/delete-project/${id}`);
+    const response = await deleteProject(id);
+    res
+      .status(200)
+      .json({ message: `Project ID:${response} has been deleted` });
+  } catch (error) {
+    console.error("/delete-project", error.message);
   }
 });
 
