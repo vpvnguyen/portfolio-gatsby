bf53408258b4413a53d516be3ebc639a05d08077
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-26 21:05:21 -0700

    styled and refined welcome message

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -1,41 +1,29 @@
 import React from "react";
 
+const style = {
+  welcomeWrapper: {
+    minHeight: "91vh",
+  },
+  heading: {
+    paddingTop: 50,
+    paddingLeft: 50,
+  },
+  intro: {
+    paddingTop: 100,
+    paddingLeft: 50,
+  },
+};
+
 const Welcome = () => {
   return (
-    <>
-      <h1>Welcome component</h1>
-
-      <h1>Hi, I'm Vincent :)</h1>
-      <p>Welcome to my Portfolio.</p>
-      <p>Now go build something great.</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-      <p>Lorem</p>
-    </>
+    <div style={style.welcomeWrapper}>
+      <h1 style={style.heading}>Welcome!</h1>
+      <div style={style.intro}>
+        <h1>Hi, I'm Vincent :)</h1>
+        <p>Welcome to my Portfolio.</p>
+        <p>Great things are built here.</p>
+      </div>
+    </div>
   );
 };
 
