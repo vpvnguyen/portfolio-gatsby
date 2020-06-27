1c11052b8c658b2c0bd15a165e26e1525073613c
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-26 21:07:46 -0700

    add trycatch logic to all api calls

--- client/src/utils/api/projects.api.js
+++ client/src/utils/api/projects.api.js
@@ -1,8 +1,17 @@
 import axios from "axios";
 
 const ProjectsAPI = {
-  getAllProjects: async () =>
-    await axios.get("http://localhost:5000/api/get-all-projects"),
+  getAllProjects: async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:5000/api/get-all-projects"
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error.message);
+      return console.log("Issue fetching projects");
+    }
+  },
   uploadProject: async ({ title, description, githubUrl, demoUrl }) => {
     try {
       const response = await axios.post(
