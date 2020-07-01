6fc691eb213abeb318e935e601a8b70d53652eef
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-30 19:03:16 -0700

    created blog component

---
+++ client/src/components/Blog/Blog.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const style = {
+  blogWrapper: {
+    border: "0.25px solid",
+    borderRadius: "20px",
+  },
+  blogContent: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  blogLine: {
+    width: "70%",
+    opacity: 0.25,
+  },
+};
+const Blog = () => {
+  return (
+    <div style={style.blogWrapper}>
+      <h1>This is my blog</h1>
+      <div style={style.blogContent}>
+        <hr style={style.blogLine} />
+        <h3>Post 1</h3>
+        <p>Post 1 paragraph</p>
+        <hr style={style.blogLine} />
+        <h3>Post 2</h3>
+        <p>Post 2 paragraph</p>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
