f9ade02c273418b83f6481f96a2faff14d75b1a7
Vincent Nguyen <vpvnguyen@gmail.com>
2020-06-30 19:03:00 -0700

    removed viewheight wrapper

--- client/src/ui/Layout/Layout.js
+++ client/src/ui/Layout/Layout.js
@@ -7,30 +7,18 @@ import { CssBaseline, Container } from "@material-ui/core";
 // components
 import Header from "../Header.js";
 import Navbar from "../../components/Navbar/Navbar.js";
-import SideBar from "../../components/SideBar/SideBar.js";
 import Footer from "../Footer/Footer.js";
-
-const style = {
-  contentWrapper: {
-    height: "100vh", // fallback
-    height: "calc(var(--vh, 1vh) * 100)",
-    margin: "0 auto",
-  },
-};
 
 const Layout = ({ children }) => {
   return (
     <>
       <CssBaseline />
       <Header />
-      <div style={style.contentWrapper}>
-        <Navbar />
-        <Container maxWidth="sm">
-          <SideBar />
-          <main>{children}</main>
-        </Container>
-        <Footer />
-      </div>
+      <Navbar />
+      <Container maxWidth="md">
+        <main>{children}</main>
+      </Container>
+      <Footer />
     </>
   );
 };
