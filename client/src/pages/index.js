687c3f70f594813e72465fceee69b23f62ee26e1
Vincent Nguyen <vpvnguyen@gmail.com>
2020-07-03 11:24:38 -0700

    consolidated style measurements using rem

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -7,52 +7,39 @@ import Image from "../images/image.js";
 import Welcome from "../components/Welcome/Welcome.js";
 import Blog from "../components/Blog/Blog.js";
 import ListProjects from "../components/ListProjects/ListProjects.js";
+import Footer from "../components/Footer/Footer.js";
 
 const style = {
-  contentWrapper: {
-    height: "100vh", // fallback
-    // width: "100vw",
-    margin: "0 auto",
+  page: {
+    height: "100vh",
   },
   defaultImage: {
     maxWidth: `300px`,
     marginBottom: `1.45rem`,
   },
-  sectionOne: {
+  landing: {
     height: "89%",
   },
-  sectionTwo: {
-    height: "50%",
-  },
-  sectionThree: {
-    height: "100%",
-  },
-  sectionFour: {
-    height: "100%",
-  },
-  sectionFive: {
-    height: "100%",
+  content: {
+    display: "block",
+    overflow: "auto",
   },
 };
 
 const IndexPage = () => (
   <Layout>
-    <div style={style.contentWrapper}>
-      <div style={style.sectionOne}>
+    <div style={style.page}>
+      <div style={style.landing}>
         <Welcome />
       </div>
 
-      <div style={style.sectionTwo}>
-        <hr />
-        <Blog />
-      </div>
+      <Blog style={style.content} />
 
-      <div style={style.sectionThree}>
-        <ListProjects />
-      </div>
+      <ListProjects style={style.content} />
 
-      <div style={style.sectionFour}>
-        <div>
+      <div style={style.content}>
+        <div style={style.defaultImage}>
+          <Image />
           <Link to="/page-2/">Go to page 2</Link>
           <br />
           <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
@@ -63,11 +50,7 @@ const IndexPage = () => (
         </div>
       </div>
 
-      <div style={style.sectionFive}>
-        <div style={style.defaultImage}>
-          <Image />
-        </div>
-      </div>
+      <Footer />
     </div>
   </Layout>
 );
