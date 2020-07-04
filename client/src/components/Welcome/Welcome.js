4a055633389323266bdc4f5331445916c635740c
Vincent Nguyen <vpvnguyen@gmail.com>
2020-07-03 19:45:56 -0700

    removed styling; add about me section

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -1,36 +1,23 @@
 import React from "react";
 
-const style = {
-  welcomeWrapper: {
-    // minHeight: "92vh",
-    // // height: "20%",
-    // display: "flex",
-    // alignItems: "center",
-    // justifyContent: "center",
-    // flexDirection: "column",
-    // paddingBottom: 650,
-  },
-  heading: {
-    // paddingTop: -50,
-    // paddingLeft: 50,
-    // paddingBottom: 50,
-  },
-  intro: {
-    // paddingTop: 50,
-    // paddingLeft: 50,
-  },
-};
-
 const Welcome = () => {
   return (
-    <div style={style.welcomeWrapper}>
-      <h1 style={style.heading}>Welcome!</h1>
-      <div style={style.intro}>
-        <h1>Hi, I'm Vincent :)</h1>
-        <p>Welcome to my Portfolio.</p>
-        <p>Great things are built here.</p>
-      </div>
-    </div>
+    <>
+      <h1>Welcome!</h1>
+      <h1>Hi, I'm Vincent :)</h1>
+      <p>Welcome to my Portfolio.</p>
+      <p>Great things are built here.</p>
+
+      <AboutMe />
+    </>
+  );
+};
+
+const AboutMe = () => {
+  return (
+    <>
+      <h1>About Me Header</h1>
+    </>
   );
 };
 
