9210cfb95447e7f84e5c4cf37ad457a95fdec4d6
vpvnguyen <vpvnguyen@gmail.com>
2020-07-05 16:28:00 -0700

    add layout to entire page

--- admin/src/pages/projects.js
+++ admin/src/pages/projects.js
@@ -1,13 +1,17 @@
 import React from "react"
+
+import Layout from "../components/layout.js"
 import UploadProject from "../components/UploadProject.js"
 import ListProject from "../components/ListProjects.js"
 
+const style = {}
+
 const Projects = () => {
   return (
-    <>
+    <Layout>
       <ListProject />
       <UploadProject />
-    </>
+    </Layout>
   )
 }
 
