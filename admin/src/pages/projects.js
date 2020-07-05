22ca5a8c7bb031461d4fe2a4aee51eb6a0279dc5
vpvnguyen <vpvnguyen@gmail.com>
2020-07-05 15:34:14 -0700

    render list and upload project components

---
+++ admin/src/pages/projects.js
@@ -0,0 +1,14 @@
+import React from "react"
+import UploadProject from "../components/UploadProject.js"
+import ListProject from "../components/ListProjects.js"
+
+const Projects = () => {
+  return (
+    <>
+      <ListProject />
+      <UploadProject />
+    </>
+  )
+}
+
+export default Projects
