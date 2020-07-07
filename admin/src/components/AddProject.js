53cd5ac7085c98bdcfb7362bc305d1566d9abec6
vpvnguyen <vpvnguyen@gmail.com>
2020-07-07 16:16:30 -0700

    create component to add projects

---
+++ admin/src/components/AddProject.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const AddProject = () => {
+  return (
+    <>
+      <h1>Add Project</h1>
+      <p>Select a project to be edited</p>
+      <p>Add title</p>
+      <p>Upload an image/gif or link to video</p>
+      <p>Save info to DB</p>
+      <p>Data in DB to be served to server</p>
+    </>
+  )
+}
+
+export default AddProject
