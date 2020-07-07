123eced44ca7d791bb7eec60b9b6f51c5b534f94
vpvnguyen <vpvnguyen@gmail.com>
2020-07-07 00:03:43 -0700

    removed unncessary links

--- admin/src/pages/index.js
+++ admin/src/pages/index.js
@@ -8,9 +8,6 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Vincent Nguyen" />
     <Link to="/projects/">Manage Projects</Link>
-    <br />
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     <h1>Admin Panel</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
