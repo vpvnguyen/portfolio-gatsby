71fb9073987d44264dab090c7f5abc3b23a89948
Vincent Nguyen <vpvnguyen@gmail.com>
2020-07-08 17:54:32 -0700

    removed styling

--- admin/src/pages/projects.js
+++ admin/src/pages/projects.js
@@ -3,21 +3,12 @@ import React from "react"
 import Layout from "../components/layout.js"
 import UploadProject from "../components/UploadProject.js"
 import ListProject from "../components/ListProjects.js"
-import AddProject from "../components/AddProject.js"
-
-const style = {
-  display: "block",
-  overflow: "auto",
-}
 
 const Projects = () => {
   return (
     <Layout>
-      <div style={style}>
-        <AddProject />
-        <ListProject />
-        <UploadProject />
-      </div>
+      <ListProject />
+      <UploadProject />
     </Layout>
   )
 }
