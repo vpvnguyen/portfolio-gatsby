dab7f7bb9b49f71b0cb179ff452fe9a2be280bac
Vincent Nguyen <vpvnguyen@gmail.com>
2020-08-22 15:53:17 -0700

    get github project langauges

--- client/src/utils/api/github.api.js
+++ client/src/utils/api/github.api.js
@@ -12,6 +12,17 @@ const GithubAPI = {
       return console.log("Issue fetching Github Projects");
     }
   },
+  getProjectLanguages: async projectName => {
+    try {
+      const response = await axios.get(
+        `https://api.github.com/repos/vpvnguyen/${projectName}/languages`
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error.message);
+      return console.log("Issue fetching Github Project languages");
+    }
+  },
 };
 
 export default GithubAPI;
