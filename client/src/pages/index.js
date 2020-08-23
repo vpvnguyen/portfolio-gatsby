752d62b2d03aac9bed415770166ae3996f52fc24
Vincent Nguyen <vpvnguyen@gmail.com>
2020-08-23 09:42:09 -0700

    removed gatsby placeholder

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { Link } from "gatsby";
 
 import Layout from "../ui/Layout/Layout.js";
-import Image from "../images/image.js";
 
 import Welcome from "../components/Welcome/Welcome.js";
 import Blog from "../components/Blog/Blog.js";
@@ -36,17 +34,6 @@ const IndexPage = () => (
       <Blog style={style.content} />
 
       <ListProjects style={style.content} />
-
-      <div style={style.content}>
-        <div style={style.defaultImage}>
-          <Image />
-          <Link to="/page-2/">Go to page 2</Link>
-          <br />
-          <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-          <br />
-          <Link to="/projects/">Projects</Link>
-        </div>
-      </div>
 
       <Footer />
     </div>
