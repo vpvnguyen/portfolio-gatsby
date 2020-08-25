3f286f812081bb61f0ab5bba528b89901e158de7
Vincent Nguyen <vpvnguyen@gmail.com>
2020-08-24 23:26:24 -0700

    pass site title to header and navbar component

--- client/src/ui/Layout/Layout.js
+++ client/src/ui/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
 
 // material ui
 import { CssBaseline, Container } from "@material-ui/core";
@@ -9,11 +10,21 @@ import Header from "../Header.js";
 import Navbar from "../../components/Navbar/Navbar.js";
 
 const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query siteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
   return (
     <>
       <CssBaseline />
-      <Header />
-      <Navbar />
+      <Header title={data.site.siteMetadata.title} />
+      <Navbar name={data.site.siteMetadata.title} />
       <Container maxWidth="md">
         <main>{children}</main>
       </Container>
