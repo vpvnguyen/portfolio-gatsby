9059d70655a28338579aba379c9aa127b1f54be3
Vincent Nguyen <vpvnguyen@gmail.com>
2020-08-24 23:25:58 -0700

    set page header to name

--- client/src/ui/Header.js
+++ client/src/ui/Header.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-const Header = () => {
-  return <Helmet title="Vincent Nguyen" />;
+const Header = ({ title }) => {
+  return <Helmet title={title} />;
 };
 
 export default Header;
