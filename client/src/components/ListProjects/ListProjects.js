f473ad7abbf57791d4ec5f8eefb0a12338d578e2
Vincent Nguyen <vpvnguyen@gmail.com>
2020-08-30 11:09:43 -0700

    add centered loader when projects are rendering

--- client/src/components/ListProjects/ListProjects.js
+++ client/src/components/ListProjects/ListProjects.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "@material-ui/core";
+import { Button, CircularProgress } from "@material-ui/core";
 import Languages from "../ListProjects/Languages";
 import dayjs from "dayjs";
 
 import GithubAPI from "../../utils/api/github.api.js";
 
 const style = {
+  loader: {
+    display: "flex",
+    justifyContent: "center",
+    padding: "40px",
+  },
   container: {
     display: "flex",
     flexDirection: "column",
@@ -50,29 +55,33 @@ const ListProjects = () => {
     <>
       <h1>Projects</h1>
       <div style={style.container}>
-        {githubProjects
-          ? githubProjects.map(project => (
-              <Button
-                key={project.name}
-                style={style.body}
-                href={project.html_url}
-                target="_blank"
-                rel="noreferrer"
-                fullWidth
-              >
-                <div style={style.left}>
-                  <h3>{project.name}</h3>
-                  <div style={style.subtext}>{project.description}</div>
+        {githubProjects ? (
+          githubProjects.map(project => (
+            <Button
+              key={project.name}
+              style={style.body}
+              href={project.html_url}
+              target="_blank"
+              rel="noreferrer"
+              fullWidth
+            >
+              <div style={style.left}>
+                <h3>{project.name}</h3>
+                <div style={style.subtext}>{project.description}</div>
+              </div>
+              <div style={style.right}>
+                <Languages style={style.subtext} projectName={project.name} />
+                <div style={style.subtext}>
+                  {dayjs(project.pushed_at).format("MMM/YYYY")}
                 </div>
-                <div style={style.right}>
-                  <Languages style={style.subtext} projectName={project.name} />
-                  <div style={style.subtext}>
-                    {dayjs(project.pushed_at).format("MMM/YYYY")}
-                  </div>
-                </div>
-              </Button>
-            ))
-          : `Loading...`}
+              </div>
+            </Button>
+          ))
+        ) : (
+          <div style={style.loader}>
+            <CircularProgress />
+          </div>
+        )}
       </div>
     </>
   );
