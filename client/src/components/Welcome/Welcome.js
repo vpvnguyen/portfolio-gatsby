c847bb4fa21b58ccd540d3e237d2d8f37f05f1eb
vpvnguyen <vpvnguyen@gmail.com>
2020-09-02 15:42:20 -0700

    import background component

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -1,38 +1,18 @@
 import React from "react";
-import BackgroundImage from "../../hb.jpg";
 import LayoutStyle from "../../ui/Layout/Layout.style";
-
-const style = {
-  background: {
-    backgroundImage: `url(${BackgroundImage})`,
-    /* Background image is centered vertically and horizontally at all times */
-    backgroundPosition: "center center",
-    /* Background image doesn't tile */
-    backgroundRepeat: "no-repeat",
-    /* Background image is fixed in the viewport so that it doesn't move when
-       the content's height is greater than the image's height */
-    backgroundAttachment: "fixed",
-    /* This is what makes the background image rescale based
-       on the container's size */
-    backgroundSize: "cover",
-    /* Set a background color that will be displayed
-       while the background image is loading */
-    backgroundColor: "#fafafa",
-  },
-};
+import Background from "../../ui/Background";
 
 const Welcome = () => {
   return (
-    <div style={style.background}>
+    <Background>
       <LayoutStyle>
         <h1>Welcome!</h1>
         <h1>Hi, I'm Vincent :)</h1>
         <p>Welcome to my Portfolio.</p>
         <p>Great things are built here.</p>
-
         <AboutMe />
       </LayoutStyle>
-    </div>
+    </Background>
   );
 };
 
