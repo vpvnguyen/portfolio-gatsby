8c90ae348542cad1fb4b9a7dc723426412169e89
vpvnguyen <vpvnguyen@gmail.com>
2020-09-04 14:07:25 -0700

    fix background position

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -2,15 +2,33 @@ import React from "react";
 import LayoutStyle from "../../ui/Layout/Layout.style";
 import Background from "../../ui/Background";
 
+const style = {
+  container: {
+    color: "#FAFAFA",
+    marginTop: "-20px",
+    padding: "100px 0px 200px 0",
+  },
+};
+
 const Welcome = () => {
   return (
     <Background>
       <LayoutStyle>
-        <h1>Welcome!</h1>
-        <h1>Hi, I'm Vincent :)</h1>
-        <p>Welcome to my Portfolio.</p>
-        <p>Great things are built here.</p>
-        <AboutMe />
+        <div style={style.container}>
+          <h1>Welcome!</h1>
+          <h1>Hi, I'm Vincent :)</h1>
+          <p>Welcome to my Portfolio.</p>
+          <p>Great things are built here.</p>
+          <p>Great things are built here.</p>
+          <p>Great things are built here.</p>
+          <p>Great things are built here.</p>
+          <p>Great things are built here.</p>
+          <p>Great things are built here.</p>
+          <p>Great things are built here.</p>
+          <p>Great things are built here.</p>
+          <p>Great things are built here.</p>
+          <AboutMe />
+        </div>
       </LayoutStyle>
     </Background>
   );
