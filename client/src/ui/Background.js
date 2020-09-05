8b06df3d4a9c940c01a4f8d4f70b79705d271f20
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-05 09:30:11 -0700

    darken background for 8 to 1 contrast accessibility

--- client/src/ui/Background.js
+++ client/src/ui/Background.js
@@ -7,7 +7,7 @@ const style = {
     /* top, transparent black, faked with gradient */
     backgroundImage: `
       linear-gradient(
-      rgba(0, 0, 0, 0.3), 
+      rgba(0, 0, 0, 0.7), 
       rgba(0, 0, 0, 0.8)
       ),
       url(${BackgroundImage})`,
