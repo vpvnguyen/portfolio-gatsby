5c105766f31da123966a39680bdf2f61f4a9b085
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-05 10:17:46 -0700

    separated colors into light, dark, accent

--- client/src/ui/theme.js
+++ client/src/ui/theme.js
@@ -1,6 +1,23 @@
 const theme = {
-  primary: "#282828",
-  secondary: "#03DAC5",
+  color: {
+    light: "#FAFAFA",
+    dark: "#282828",
+    accent: "#03DAC5",
+  },
+  languages: {
+    default: "#60656C",
+    html: "#FFF4E6",
+    css: "#E7F5FF",
+    javascript: "#FFF3BF",
+    typescript: "#00273F",
+    php: "#F3F0FF",
+    ruby: "#CC342D",
+    python: "#3776AB",
+    java: "#940000",
+    ["objective-c"]: "#323F50",
+    sql: "#FFD43B",
+    tsql: "#00273F",
+  },
 };
 
 export default theme;
