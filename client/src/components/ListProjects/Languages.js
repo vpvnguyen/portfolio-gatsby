6ff17a3a0299c680d0efe3b03f2f2b0db4ef57b1
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-06 11:38:32 -0700

    moved all methods to map language styles within useEffect

--- client/src/components/ListProjects/Languages.js
+++ client/src/components/ListProjects/Languages.js
@@ -7,43 +7,43 @@ import theme from "../../ui/theme";
 const Languages = ({ projectName }) => {
   const [languages, setLanguages] = useState([]);
 
-  const fetchGithubProjectLanguage = async projectName => {
-    const githubLanguages = await GithubAPI.getProjectLanguages(projectName);
-    const languagesLowerCase = Object.keys(githubLanguages).map(value =>
-      value.toLowerCase()
-    );
-    const languageArray = await mapLanguageStyle(languagesLowerCase);
-    setLanguages(languageArray);
-  };
-
-  const mapLanguageStyle = languageArray => {
-    const languageColorKey = Object.keys(theme.languages);
-    return mapLanguageColor(languageArray, languageColorKey);
-  };
-
-  const mapLanguageColor = (languageArray, languageColorKey) =>
-    languageArray.map((value, index) => {
-      let style = {
-        fontSize: ".75em",
-        marginLeft: "3px",
-        padding: "5px",
-        borderRadius: "20px",
-        background: "white",
-        color: "black",
-      };
-
-      if (languageColorKey.includes(value)) {
-        style.background = theme.languages[value].background;
-        style.color = theme.languages[value].color;
-        return { name: languageArray[index], style };
-      }
-
-      style.background = theme.languages.default.background;
-      style.color = theme.languages.default.color;
-      return { name: languageArray[index], style };
-    });
-
   useEffect(() => {
+    const fetchGithubProjectLanguage = async projectName => {
+      const githubLanguages = await GithubAPI.getProjectLanguages(projectName);
+      const languagesLowerCase = Object.keys(githubLanguages).map(value =>
+        value.toLowerCase()
+      );
+      const languageArray = await mapLanguageStyle(languagesLowerCase);
+      setLanguages(languageArray);
+    };
+
+    const mapLanguageStyle = languageArray => {
+      const languageColorKey = Object.keys(theme.languages);
+      return mapLanguageColor(languageArray, languageColorKey);
+    };
+
+    const mapLanguageColor = (languageArray, languageColorKey) =>
+      languageArray.map((value, index) => {
+        let style = {
+          fontSize: ".75em",
+          marginLeft: "3px",
+          padding: "5px",
+          borderRadius: "20px",
+          background: "white",
+          color: "black",
+        };
+
+        if (languageColorKey.includes(value)) {
+          style.background = theme.languages[value].background;
+          style.color = theme.languages[value].color;
+          return { name: languageArray[index], style };
+        }
+
+        style.background = theme.languages.default.background;
+        style.color = theme.languages.default.color;
+        return { name: languageArray[index], style };
+      });
+
     fetchGithubProjectLanguage(projectName);
   }, [projectName]);
 
