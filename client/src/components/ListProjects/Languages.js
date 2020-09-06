23d57cbcf98458bfa4ff41edbb391950b46a74d7
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-06 11:14:34 -0700

    styled languages with respective colors

--- client/src/components/ListProjects/Languages.js
+++ client/src/components/ListProjects/Languages.js
@@ -4,49 +4,46 @@ import { CircularProgress } from "@material-ui/core";
 
 import theme from "../../ui/theme";
 
-const style = {
-  langaugeColor: `${theme.languages.default}`,
-  languages: {
-    fontSize: ".75em",
-    marginLeft: "3px",
-    padding: "5px",
-    borderRadius: "20px",
-    border: `1px solid #03dac5`,
-    background: "white",
-  },
-};
-
 const Languages = ({ projectName }) => {
   const [languages, setLanguages] = useState([]);
-  const [languageColor, setLanguageColor] = useState([]);
 
-  const getLanguageColor = array => {
-    const key = Object.keys(theme.languages);
-
-    const languageColors = array.map((value, index) => {
-      if (key.includes(value))
-        return { language: array[index], color: theme.languages[value] };
-      return { language: array[index], color: theme.languages.default };
-    });
-
-    console.log(languageColors);
-    setLanguageColor(languageColors);
+  const fetchGithubProjectLanguage = async projectName => {
+    const githubLanguages = await GithubAPI.getProjectLanguages(projectName);
+    const languagesLowerCase = Object.keys(githubLanguages).map(value =>
+      value.toLowerCase()
+    );
+    const languageArray = await mapLanguageStyle(languagesLowerCase);
+    setLanguages(languageArray);
   };
 
-  console.log("theme color", Object.keys(theme.languages));
-  console.log("style", style.langaugeColor);
-  useEffect(() => {
-    const fetchGithubProjectLanguage = async projectName => {
-      const githubLanguages = await GithubAPI.getProjectLanguages(projectName);
-      const languagesLowerCase = Object.keys(githubLanguages).map(value =>
-        value.toLowerCase()
-      );
-      setLanguages(languagesLowerCase);
-      console.log("language to lower", languagesLowerCase);
-      console.log("state", languages);
-      getLanguageColor(languages);
-    };
+  const mapLanguageStyle = languageArray => {
+    const languageColorKey = Object.keys(theme.languages);
+    return mapLanguageColor(languageArray, languageColorKey);
+  };
 
+  const mapLanguageColor = (languageArray, languageColorKey) =>
+    languageArray.map((value, index) => {
+      let style = {
+        fontSize: ".75em",
+        marginLeft: "3px",
+        padding: "5px",
+        borderRadius: "20px",
+        background: "white",
+        color: "black",
+      };
+
+      if (languageColorKey.includes(value)) {
+        style.background = theme.languages[value].background;
+        style.color = theme.languages[value].color;
+        return { name: languageArray[index], style };
+      }
+
+      style.background = theme.languages.default.background;
+      style.color = theme.languages.default.color;
+      return { name: languageArray[index], style };
+    });
+
+  useEffect(() => {
     fetchGithubProjectLanguage(projectName);
   }, [projectName]);
 
@@ -55,23 +52,8 @@ const Languages = ({ projectName }) => {
       {languages ? (
         <div>
           {languages.map((language, index) => (
-            <span key={`${projectName}-${index}`} style={style.languages}>
-              {/* {if (language)} */}
-              {language}
-            </span>
-          ))}
-        </div>
-      ) : (
-        <CircularProgress />
-      )}
-      {/* WIP - render language and its color */}
-      {languageColor ? (
-        <div>
-          {languageColor.map((value, index) => (
-            <span key={`${projectName}-${index}`} style={style.languageColor}>
-              {/* {if (language)} */}
-              {value.language}
-              {value.color}
+            <span key={`${projectName}-${index}`} style={language.style}>
+              {language.name}
             </span>
           ))}
         </div>
