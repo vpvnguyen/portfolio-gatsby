18df3b84f4cfa52ce5e0bf13752ba12555740b2a
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-07 20:25:59 -0700

    create twitter config with creds

---
+++ server/config/twitter.config.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  bearer_token: process.env.TWITTER_BEARER_TOKEN,
+});
+
+module.exports = client;
