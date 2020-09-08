bd07e615f991d16c3fd0bd2b905ba7e6b5f0ca12
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-07 20:20:56 -0700

    removed time controller

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use("/time", require("./controller/time.controller"));
 app.use("/api", require("./controller/projects.controller"));
 app.use("/api", require("./controller/twitter.controller"));
 
