3fa853b97e5c9498af5f2e45afa42d851c6ad21b
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-07 20:25:40 -0700

    import twitter client

--- server/controller/twitter.controller.js
+++ server/controller/twitter.controller.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
 const express = require("express");
 const router = express.Router();
+const twitter = require("../config/twitter.config");
 
 router.get("/twitter", (req, res) => {
   res.json({ message: "twitter" });
