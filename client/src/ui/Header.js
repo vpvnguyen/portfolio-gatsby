b04e4cd3b9ccded5831dc0df1b11ea7e477c75ca
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-09 19:00:08 -0700

    set css baseline when header loads

--- client/src/ui/Header.js
+++ client/src/ui/Header.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { CssBaseline } from "@material-ui/core";
 
 const Header = ({ title }) => {
-  return <Helmet title={title} />;
+  return (
+    <>
+      <CssBaseline />
+      <Helmet title={title} />
+    </>
+  );
 };
 
 export default Header;
