783b79a53b85321444492376217b9cbc867b2466
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-09 19:02:00 -0700

    delete blog component and twitter controller

--- server/controller/twitter.controller.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const twitter = require("../config/twitter.config");
-
-router.get("/twitter", (req, res) => {
-  res.json({ message: "twitter" });
-});
-
-module.exports = router;
