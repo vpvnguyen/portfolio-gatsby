381a245615f977fd89fe4b68ebcb9c2581950713
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-10 23:01:28 -0700

    removed bullet points on list

--- client/src/components/Welcome/AboutMe.js
+++ client/src/components/Welcome/AboutMe.js
@@ -1,10 +1,16 @@
 import React from "react";
 
+const style = {
+  list: {
+    listStyleType: "none",
+  },
+};
+
 const AboutMe = () => {
   return (
     <>
       <h1>Software Engineer</h1>
-      <ul>
+      <ul style={style.list}>
         <li>Quality Assurance</li>
         <li>Test Automation</li>
         <li>Pipeline Management</li>
