42e979a7e42859a2dac089a9d73db57ac341808a
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-10 22:56:04 -0700

    refactored and created list of qualities

---
+++ client/src/components/Welcome/AboutMe.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <>
+      <h1>Software Engineer</h1>
+      <ul>
+        <li>Quality Assurance</li>
+        <li>Test Automation</li>
+        <li>Pipeline Management</li>
+        <li>PCI Security Standards</li>
+      </ul>
+    </>
+  );
+};
+
+export default AboutMe;
