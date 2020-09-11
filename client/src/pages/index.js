f3bd263e7a2e419c5019698e9148646b7c0c9c4e
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-10 22:27:27 -0700

    refactored flexbox styling; add background component

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,6 +4,7 @@ import Welcome from "../components/Welcome/Welcome.js";
 import ListProjects from "../components/ListProjects/ListProjects.js";
 import Footer from "../components/Footer/Footer.js";
 import Twitter from "../components/Blog/Twitter";
+import Background from "../ui/Background";
 
 const style = {
   page: {
@@ -15,15 +16,24 @@ const style = {
   container: {
     display: "flex",
     flexWrap: "wrap",
+    justifyContent: "space-around",
+  },
+  content: {
+    display: "flex",
+    flexWrap: "wrap",
+    maxWidth: "1280px",
   },
   static: {
     flex: "1 100%",
+    textAlign: "center",
   },
   left: {
     flex: 2,
+    padding: "1rem 2rem 1rem 2rem",
   },
   right: {
     flex: 1,
+    padding: "1rem 2rem 1rem 2rem",
   },
 };
 
@@ -33,13 +43,17 @@ const IndexPage = () => {
       <div style={style.page}>
         <div style={style.container}>
           <div style={style.static}>
-            <Welcome />
+            <Background>
+              <Welcome />
+            </Background>
           </div>
-          <div style={style.left}>
-            <ListProjects />
-          </div>
-          <div style={style.right}>
-            <Twitter />
+          <div style={style.content}>
+            <div style={style.left}>
+              <ListProjects />
+            </div>
+            <div style={style.right}>
+              <Twitter />
+            </div>
           </div>
         </div>
         <Footer />
