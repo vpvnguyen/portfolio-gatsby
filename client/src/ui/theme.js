b2623d8c778f6315df198f73abe4ad8252e7fb0b
vpvnguyen <vpvnguyen@gmail.com>
2020-09-11 16:16:39 -0700

    lighten java background color

--- client/src/ui/theme.js
+++ client/src/ui/theme.js
@@ -9,11 +9,11 @@ const theme = {
     html: { color: "black", border: "#FFF4E6", background: "#fff0dd" },
     css: { color: "black", border: "#E7F5FF", background: "#E7F5FF" },
     javascript: { color: "black", border: "#FFF3BF", background: "#FFF3BF" },
-    typescript: { color: "black", border: "#00273F", background: "#d5E2EC" },
+    typescript: { color: "black", border: "#00273F", background: "#D5E2EC" },
     php: { color: "black", border: "#F3F0FF", background: "#F3F0FF" },
     ruby: { color: "black", border: "#CC342D", background: "#FFD6D6" },
     python: { color: "black", border: "#3776AB", background: "#D5E2EC" },
-    java: { color: "black", border: "#940000", background: "#dBC5C5" },
+    java: { color: "black", border: "#940000", background: "#EBDDDD" },
     ["objective-c"]: {
       color: "black",
       border: "#323F50",
