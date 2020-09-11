c38391b3db759e6ab1059eec046b383364ec20d3
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-10 22:26:25 -0700

    removed styling

--- client/src/components/Blog/Twitter.js
+++ client/src/components/Blog/Twitter.js
@@ -3,14 +3,6 @@ import { useStaticQuery, graphql } from "gatsby";
 import useScript from "../../utils/hooks/useScript";
 import LayoutStyle from "../../ui/Layout/Layout.style";
 
-const style = {
-  container: {
-    display: "flex",
-    maxWidth: "50%",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-};
 const EmbedTwitterTimeline = (url, user) => {
   const data = useStaticQuery(graphql`
     query siteTwitterQuery {
@@ -30,14 +22,12 @@ const EmbedTwitterTimeline = (url, user) => {
   useScript("https://platform.twitter.com/widgets.js");
   return (
     <LayoutStyle>
-      <div style={style.container}>
-        <a
-          class="twitter-timeline"
-          href={data.site.siteMetadata.social.twitter.url}
-        >
-          Tweets by {data.site.siteMetadata.social.twitter.user}
-        </a>
-      </div>
+      <a
+        class="twitter-timeline"
+        href={data.site.siteMetadata.social.twitter.url}
+      >
+        Tweets by {data.site.siteMetadata.social.twitter.user}
+      </a>
     </LayoutStyle>
   );
 };
