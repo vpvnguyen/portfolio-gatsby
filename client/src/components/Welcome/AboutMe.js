fced968611dacc8688ac92816f6a3156823c3c90
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-10 23:04:41 -0700

    removed list and styling

--- client/src/components/Welcome/AboutMe.js
+++ client/src/components/Welcome/AboutMe.js
@@ -1,21 +1,13 @@
 import React from "react";
 
-const style = {
-  list: {
-    listStyleType: "none",
-  },
-};
-
 const AboutMe = () => {
   return (
     <>
       <h1>Software Engineer</h1>
-      <ul style={style.list}>
-        <li>Quality Assurance</li>
-        <li>Test Automation</li>
-        <li>Pipeline Management</li>
-        <li>PCI Security Standards</li>
-      </ul>
+      <p>Quality Assurance</p>
+      <p>Test Automation</p>
+      <p>Pipeline Management</p>
+      <p>PCI Security Standards</p>
     </>
   );
 };
