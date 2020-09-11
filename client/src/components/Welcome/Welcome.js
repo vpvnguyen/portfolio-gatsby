eed2d18f2d7591f267408bdbd29be944390b8c48
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-10 22:26:53 -0700

    removed background component

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -1,6 +1,5 @@
 import React from "react";
 import LayoutStyle from "../../ui/Layout/Layout.style";
-import Background from "../../ui/Background";
 
 const style = {
   container: {
@@ -11,25 +10,23 @@ const style = {
 
 const Welcome = () => {
   return (
-    <Background>
-      <LayoutStyle>
-        <div style={style.container}>
-          <h1>Welcome!</h1>
-          <h1>Hi, I'm Vincent :)</h1>
-          <p>Welcome to my Portfolio.</p>
-          <p>Great things are built here.</p>
-          <p>Great things are built here.</p>
-          <p>Great things are built here.</p>
-          <p>Great things are built here.</p>
-          <p>Great things are built here.</p>
-          <p>Great things are built here.</p>
-          <p>Great things are built here.</p>
-          <p>Great things are built here.</p>
-          <p>Great things are built here.</p>
-          <AboutMe />
-        </div>
-      </LayoutStyle>
-    </Background>
+    <LayoutStyle>
+      <div style={style.container}>
+        <h1>Welcome!</h1>
+        <h1>Hi, I'm Vincent :)</h1>
+        <p>Welcome to my Portfolio.</p>
+        <p>Great things are built here.</p>
+        <p>Great things are built here.</p>
+        <p>Great things are built here.</p>
+        <p>Great things are built here.</p>
+        <p>Great things are built here.</p>
+        <p>Great things are built here.</p>
+        <p>Great things are built here.</p>
+        <p>Great things are built here.</p>
+        <p>Great things are built here.</p>
+        <AboutMe />
+      </div>
+    </LayoutStyle>
   );
 };
 
