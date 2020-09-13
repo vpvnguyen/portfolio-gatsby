215511c3fa58afd0a596de9c93da2befb56181a7
vpvnguyen <vpvnguyen@gmail.com>
2020-09-12 22:42:02 -0700

    moved backend logic to query and map for projects to frontend api call

--- client/src/utils/api/github.api.js
+++ client/src/utils/api/github.api.js
@@ -23,6 +23,45 @@ const GithubAPI = {
       return null;
     }
   },
+  fetchGithubProjects: async () => {
+    try {
+      // get repos; 30 results per page; set per_page=100 to query for 100 pages;
+      const githubProjects = await axios.get(
+        `https://api.github.com/users/vpvnguyen/repos?per_page=100`
+      );
+
+      const filteredStarredProjects = githubProjects.data.reduce(
+        (result, project) => {
+          if (project.stargazers_count > 0) result.push(project);
+          return result;
+        },
+        []
+      );
+
+      const createProjectsArray = filteredStarredProjects.map(project => {
+        return {
+          id: project.id,
+          name: project.name,
+          description: project.description,
+          html_url: project.html_url,
+          homepage: project.homepage,
+          language: project.language,
+          created_at: project.created_at,
+          updated_at: project.updated_at,
+          pushed_at: new Date(project.pushed_at),
+        };
+      });
+
+      const sortProjectsByDate = createProjectsArray.sort(
+        (a, b) => b.pushed_at - a.pushed_at
+      );
+
+      return sortProjectsByDate;
+    } catch (error) {
+      console.error(error.message);
+      return new Error("Issue fetching github projects");
+    }
+  },
 };
 
 export default GithubAPI;
