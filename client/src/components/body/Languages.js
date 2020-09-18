d70a8cb41a7e239751b835d463eae6b95ff7917a
vpvnguyen <vpvnguyen@gmail.com>
2020-09-18 13:21:52 -0700

    fix react warn: passed props into useEffect

--- client/src/components/body/Languages.js
+++ client/src/components/body/Languages.js
@@ -52,7 +52,7 @@ const Languages = ({ url, user, projectName }) => {
       });
 
     fetchGithubProjectLanguage(url, user, projectName);
-  }, [projectName]);
+  }, [projectName, url, user]);
 
   return (
     <>
