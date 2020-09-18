0817885f3ac86054b2d1e373ac1ccb554e9bb55c
vpvnguyen <vpvnguyen@gmail.com>
2020-09-18 13:23:47 -0700

    fix import path

--- client/src/ui/layout/Layout.js
+++ client/src/ui/layout/Layout.js
@@ -1,8 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
-import Header from "..../header/Header.js";
-import Navbar from "../../components/Navbar/Navbar.js";
+import Header from "../../components/header/Header.js";
+import Navbar from "../../components/header/Navbar.js";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
