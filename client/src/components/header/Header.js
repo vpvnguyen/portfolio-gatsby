d79c6df290584f89c83d2f6afadc202148df3aa7
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-22 19:28:22 -0700

    cleaned up function

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -2,13 +2,11 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import { CssBaseline } from "@material-ui/core";
 
-const Header = ({ title }) => {
-  return (
-    <>
-      <CssBaseline />
-      <Helmet title={title} />
-    </>
-  );
-};
+const Header = ({ title }) => (
+  <>
+    <CssBaseline />
+    <Helmet title={title} />
+  </>
+);
 
 export default Header;
