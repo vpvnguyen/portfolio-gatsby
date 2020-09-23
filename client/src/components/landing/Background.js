bbd93ef3ffc10c516d56e11234f98d1487dc7d9e
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-22 19:29:27 -0700

    cleaned up document and removed comments

--- client/src/components/landing/Background.js
+++ client/src/components/landing/Background.js
@@ -4,37 +4,20 @@ import BackgroundImage from "../../images/hb.jpg";
 import theme from "../../ui/theme";
 
 const style = {
-  background: {
-    /* top, transparent black, faked with gradient */
-    backgroundImage: `
+  backgroundImage: `
       linear-gradient(
       rgba(0, 0, 0, 0.7), 
       rgba(0, 0, 0, 0.8)
       ),
       url(${BackgroundImage})`,
-    /* Background image is centered vertically and horizontally at all times */
-    backgroundPosition: "center center",
-    /* Background image doesn't tile */
-    backgroundRepeat: "no-repeat",
-    /* Background image is fixed in the viewport so that it doesn't move when
-         the content's height is greater than the image's height */
-    backgroundAttachment: "fixed",
-    /* This is what makes the background image rescale based
-         on the container's size */
-    backgroundSize: "cover",
-    /* Set a background color that will be displayed
-         while the background image is loading */
-    backgroundColor: theme.color.light,
-  },
+  backgroundPosition: "center center",
+  backgroundRepeat: "no-repeat",
+  backgroundAttachment: "fixed",
+  backgroundSize: "cover",
+  backgroundColor: theme.color.light,
 };
 
-const Background = ({ children }) => {
-  return (
-    <>
-      <div style={style.background}>{children}</div>
-    </>
-  );
-};
+const Background = ({ children }) => <div style={style}>{children}</div>;
 
 Background.propTypes = {
   children: PropTypes.node.isRequired,
