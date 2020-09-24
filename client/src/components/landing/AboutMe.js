1b6a047c730c9746dc1754d3d6145087762f1f19
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-23 18:25:02 -0700

    use data from static experience query

--- client/src/components/landing/AboutMe.js
+++ client/src/components/landing/AboutMe.js
@@ -1,14 +1,19 @@
 import React from "react";
 import theme from "../../ui/theme";
+import useStaticExperienceQuery from "../../utils/hooks/useStaticExperienceQuery";
 
-const AboutMe = () => (
-  <>
-    <h1 style={theme.h1}>Software Engineer</h1>
-    <p>Quality Assurance</p>
-    <p>Test Automation</p>
-    <p>Pipeline Management</p>
-    <p>PCI Security Standards</p>
-  </>
-);
+const AboutMe = () => {
+  const data = useStaticExperienceQuery();
+
+  return (
+    <>
+      <h1 style={theme.h1}>{data.site.siteMetadata.experience.current}</h1>
+      {data &&
+        data.site.siteMetadata.experience.past.map(value => (
+          <p key={value}>{value}</p>
+        ))}
+    </>
+  );
+};
 
 export default AboutMe;
