20844c21618a06b49fb89b8a65b691aa896870b8
Vincent Nguyen <vpvnguyen@gmail.com>
2020-10-02 20:04:10 -0700

    animate icons

--- client/src/components/Social.js
+++ client/src/components/Social.js
@@ -3,38 +3,59 @@ import {
   GitHub as GitHubIcon,
   LinkedIn as LinkedInIcon,
 } from "@material-ui/icons";
+import { motion } from "framer-motion";
 import theme from "../ui/theme";
 import useStaticSocialQuery from "../utils/hooks/useStaticSocialQuery";
+import Motion from "../ui/motion";
 
 const style = {
-  fontSize: "1.4em",
-  margin: "0.2em",
-  backgroundColor: theme.color.dark,
-  color: theme.color.accent,
+  links: {
+    fontSize: "1.4em",
+    margin: ".5rem",
+    backgroundColor: theme.color.dark,
+    color: theme.color.accent,
+  },
+  motion: {
+    initial: {
+      cursor: "pointer",
+    },
+    whileHover: Motion.whileHoverScale(),
+    whileTap: {
+      color: theme.color.dark,
+    },
+  },
 };
+
+const MotionIcon = ({ children, url }) => (
+  <motion.a
+    href={url}
+    style={style.links}
+    initial={style.motion.initial}
+    whileHover={style.motion.whileHover}
+    whileTap={style.motion.whileTap}
+    rel="noreferrer"
+    target="_blank"
+  >
+    {children}
+  </motion.a>
+);
 
 const Social = () => {
   const data = useStaticSocialQuery();
 
   return (
     <>
-      <a
-        style={style}
-        href={`https://www.github.com/${data.site.siteMetadata.social.github}`}
-        rel="noreferrer"
-        target="_BLANK"
+      <MotionIcon
+        url={`${data.site.siteMetadata.social.github.url}${data.site.siteMetadata.social.github.user}`}
       >
         <GitHubIcon />
-      </a>
+      </MotionIcon>
 
-      <a
-        style={style}
-        href={`https://www.linkedin.com/in/${data.site.siteMetadata.social.linkedin}`}
-        rel="noreferrer"
-        target="_BLANK"
+      <MotionIcon
+        url={`${data.site.siteMetadata.social.linkedin.url}${data.site.siteMetadata.social.linkedin.user}`}
       >
         <LinkedInIcon />
-      </a>
+      </MotionIcon>
     </>
   );
 };
