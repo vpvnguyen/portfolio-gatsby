4987e1d867b36341302cc2fd256e4cbb154fab15
Vincent Nguyen <vpvnguyen@gmail.com>
2020-10-02 20:40:48 -0700

    create spring up motion for headers

--- client/src/ui/motion.js
+++ client/src/ui/motion.js
@@ -1,5 +1,19 @@
 class Motion {
   static whileHoverScale = () => ({ scale: 1.1 });
+  static springUpHeaders = () => ({
+    initial: {
+      opacity: 0,
+      y: 10,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        damping: 50,
+      },
+    },
+  });
 }
 
 export default Motion;
