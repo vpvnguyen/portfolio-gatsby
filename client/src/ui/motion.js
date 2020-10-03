dfc63f36a438ebce170ba34375327cdb3f4313aa
Vincent Nguyen <vpvnguyen@gmail.com>
2020-10-03 04:08:59 -0700

    add welcome and about me variants

--- client/src/ui/motion.js
+++ client/src/ui/motion.js
@@ -1,3 +1,5 @@
+import theme from "../ui/theme";
+
 class MotionStyle {
   static whileHoverScale = () => ({ scale: 1.1 });
 
@@ -27,6 +29,92 @@ class MotionStyle {
         type: "spring",
         damping: 30,
       },
+    },
+  });
+
+  static springUpAboutMeHeader = () => ({
+    initial: {
+      opacity: 0,
+      y: 10,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        damping: 50,
+        delay: 2.5,
+      },
+    },
+  });
+
+  static springUpAboutMeText = () => ({
+    initial: {
+      opacity: 0,
+      y: 10,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        damping: 100,
+        delay: 4,
+        y: {
+          velocity: -50,
+        },
+      },
+    },
+  });
+
+  static springUpWelcomeHeader = () => ({
+    initial: {
+      opacity: 0,
+      y: 10,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        damping: 80,
+      },
+    },
+  });
+
+  static springUpWelcomeText = () => ({
+    initial: {
+      opacity: 0,
+      y: 10,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        damping: 50,
+        delay: 1.5,
+      },
+    },
+  });
+
+  static scaleUpSocialIcons = () => ({
+    initial: {
+      cursor: "pointer",
+    },
+    whileHover: this.whileHoverScale(),
+    whileTap: {
+      color: theme.color.dark,
+    },
+  });
+
+  static scaleUpMenuItem = () => ({
+    initial: {
+      cursor: "pointer",
+    },
+    whileHover: this.whileHoverScale(),
+    whileTap: {
+      color: theme.color.dark,
     },
   });
 }
