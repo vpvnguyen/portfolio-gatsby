9f79d9b792f97a21031626044d978b6f547de020
Vincent Nguyen <vpvnguyen@gmail.com>
2020-10-10 21:39:11 -0700

    query for author; refactored styling and component layout

--- client/src/components/landing/Welcome.js
+++ client/src/components/landing/Welcome.js
@@ -2,22 +2,30 @@ import React from "react";
 import { motion } from "framer-motion";
 import theme from "../../ui/theme";
 import MotionStyle from "../../ui/motion";
-import useStaticTitleQuery from "../../utils/hooks/useStaticTitleQuery";
+import useStaticAuthorQuery from "../../utils/hooks/useStaticAuthorQuery";
 
 const style = {
   welcomeHeader: theme.h1,
+  welcomeHeaderContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: '1rem'
+
+  },
+  welcomeTextTitle: theme.h1,
   motionWelcomeHeader: MotionStyle.springUpWelcomeHeader(),
   motionWelcomeText: MotionStyle.springUpWelcomeText(),
 };
 
 const MotionWelcomeHeader = ({ children }) => (
-  <motion.div
+  <motion.span
     variants={style.motionWelcomeHeader}
     initial={"initial"}
     animate={"animate"}
   >
     {children}
-  </motion.div>
+  </motion.span>
 );
 
 const MotionWelcomeText = ({ children }) => (
@@ -31,16 +39,18 @@ const MotionWelcomeText = ({ children }) => (
 );
 
 const Welcome = () => {
-  const data = useStaticTitleQuery();
+  const data = useStaticAuthorQuery();
 
   return (
     <>
-      <MotionWelcomeHeader>
-        <h1 style={style.welcomeHeader}>{data.site.siteMetadata.title}</h1>
-      </MotionWelcomeHeader>
-      <MotionWelcomeText>
-        I enjoy breaking and building things.
-      </MotionWelcomeText>
+      <div style={style.welcomeHeaderContainer}>
+        <MotionWelcomeHeader>
+          <h1 style={style.welcomeHeader}>{data.site.siteMetadata.author}</h1>
+        </MotionWelcomeHeader>
+        <MotionWelcomeText>
+          I enjoy breaking and building things.
+        </MotionWelcomeText>
+      </div>
     </>
   );
 };
