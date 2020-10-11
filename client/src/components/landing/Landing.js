337bf0464983b34020e06149fa6678b974fcbee9
Vincent Nguyen <vpvnguyen@gmail.com>
2020-10-10 21:38:32 -0700

    increase padding to show more background

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -1,19 +1,14 @@
 import React from "react";
 import LayoutStyle from "../../ui/layout/Layout.style";
-import AboutMe from "./AboutMe";
 import Welcome from "./Welcome";
 
 const style = {
   container: {
     color: "#ffffff",
-    padding: "5rem 0rem 10rem 0rem",
+    padding: "10rem 0rem 15rem 0rem",
   },
   landingMessage: {
     padding: "5rem",
-  },
-  aboutMe: {
-    paddingTop: "4rem",
-    paddingBottom: "5rem",
   },
 };
 
@@ -22,10 +17,6 @@ const Landing = () => (
     <div style={style.container}>
       <div style={style.landingMessage}>
         <Welcome />
-      </div>
-
-      <div style={style.aboutMe}>
-        <AboutMe />
       </div>
     </div>
   </LayoutStyle>
